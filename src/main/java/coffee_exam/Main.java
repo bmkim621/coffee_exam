73ebc1e38ce442f111ef2875ab581ce4ff707687
bmkim621 <moonstruck0614@gmail.com>
2018-12-10 23:07:51 +0900

    CoffeeManagementUI

--- src/main/java/coffee_exam/Main.java
+++ src/main/java/coffee_exam/Main.java
@@ -1,10 +1,22 @@
 package coffee_exam;
 
+import java.awt.EventQueue;
+
+import coffee_exam.ui.CoffeeManagementUI;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					CoffeeManagementUI frame = new CoffeeManagementUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
 }
